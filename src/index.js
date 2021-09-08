d077b90ea1f1f9cdd74476ea8bda547eb9672255
Kiyas <kiiyas@yandex.ru>
2021-09-08 20:40:26 +0500

    linted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import './js/bowerman';
-import './js/character';
-import './js/daemon';
-import './js/magician';
-import './js/swordsman';
-import './js/undead';
-import './js/zombie';
+import './js/app';
